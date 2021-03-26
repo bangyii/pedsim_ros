b96f3aa979c83f4ce1d52bc6672fbb84d241ffc8
BY <tanbangyi@gmail.com>
2021-03-26 16:12:01 +0800

    finish up most features of script for adding obstacles into xml and adding actors into world file

---
+++ pedsim_simulator/scripts/generate_actor_xml.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+import sys
+
+if len(sys.argv) != 3:
+    print("Please input .world file to modify, and number of actors to add")
+    print("python3 generate_actor_xml.py /home/user/gazebo.world 30")
+    exit()
+
+world_file = sys.argv[1]
+num_agents = sys.argv[2]
+
+f = open(world_file, "r+")
+contents = f.readlines()
+
+# Find world tag
+actor_insert_line = 0
+for i in range(len(contents)):
+    if contents[i].find('<world') != -1:
+        actor_insert_line = i + 1
+        break
+
+for i in range(30):
+    actor_name = "walking" + str(i)
+    contents.insert(actor_insert_line, '\t\t</actor>\n')
+    contents.insert(actor_insert_line, '\t\t\t</plugin>\n')
+    contents.insert(actor_insert_line, '\t\t\t\t<actor_id>' + str(i) + '</actor_id>\n')
+    contents.insert(actor_insert_line, '\t\t\t<plugin name="actor' + str(i) + '_plugin" filename="libactor_plugin.so">\n')
+    contents.insert(actor_insert_line, '\t\t\t</animation>\n')
+    contents.insert(actor_insert_line, '\t\t\t\t<interpolate_x>true</interpolate_x>\n')
+    contents.insert(actor_insert_line, '\t\t\t\t<filename>walk.dae</filename>\n')
+    contents.insert(actor_insert_line, '\t\t\t<animation name="walking">\n')
+    contents.insert(actor_insert_line, '\t\t\t</skin>\n')
+    contents.insert(actor_insert_line, '\t\t\t\t<filename>walk.dae</filename>\n')
+    contents.insert(actor_insert_line, '\t\t\t<skin>\n')
+    contents.insert(actor_insert_line, '\t\t\t<pose>' + str(i) + ' 0 1.0 0 0 0</pose>\n')
+    contents.insert(actor_insert_line, '\t\t<actor name="' + actor_name + '">\n')
+    # print('<actor name="' + actor_name + '">')
+    # print('<pose>' + str(i) + ' 0 1.0 0 0 0</pose>')
+    # print('<skin>')
+    # print('<filename>walk.dae</filename>')
+    # print('</skin>')
+    # print('<animation name="walking">')
+    # print('<filename>walk.dae</filename>')
+    # print('<interpolate_x>true</interpolate_x>')
+    # print('</animation>')
+    # print('<plugin name="actor' + str(i) + '_plugin" filename="libactor_plugin.so">')
+    # print('<actor_id>' + str(i) + '</actor_id>')
+    # print('</plugin>')
+    # print('</actor>')
+
+f.seek(0)
+f.writelines(contents)
+f.truncate()
+f.close()
--- pedsim_simulator/scripts/map_to_obstacles.py
+++ pedsim_simulator/scripts/map_to_obstacles.py
@@ -1,42 +1,64 @@
+#!/usr/bin/env python
 import cv2
 import matplotlib.pyplot as plt
+import sys
 
-image = cv2.imread("playpen.pgm")
+if len(sys.argv) != 3:
+    print("Please specify map file to obtain obstacles from and scenario file to add obstacles, example: ")
+    print("python3 map_to_obstacles.py /home/user/map.pgm /home/user/scenario.xml")
+    exit()
+
+print("Open map file: " + sys.argv[1])
+print("Modify scenario file: " + sys.argv[2])
+map_file = sys.argv[1]
+xml_file = sys.argv[2]
+
+image = cv2.imread(map_file)
 image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
 
+# Adjust map image to be correct side up to reflect real world. Rotate right, mirror, then flip
+# image = cv2.rotate(image, cv2.ROTATE_90_CLOCKWISE)
+image = cv2.flip(image, 0)
+# image = cv2.flip(image, 1)
+
+f = open(xml_file, "r+")
+contents = f.readlines() # Returns array of lines, each index is a line
+
+# Find scenario tag line, then insert obstacles at the following line
+obstacle_insert_line = 0 # Insert index is the index that the inserted object will be
+for i in range(len(contents)):
+    if contents[i].find('<scenario>') != -1:
+        obstacle_insert_line = i + 1
+        break
+
+# Add new line for clarity in new XML file
+contents.insert(obstacle_insert_line, '\n')
+
+# Image processing & finding contours
+gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
 _, binary = cv2.threshold(gray, 150, 255, cv2.THRESH_BINARY_INV)
 element = cv2.getStructuringElement(cv2.MORPH_RECT, (5,5))
-#binary = cv2.dilate(binary, element)
-
 contours, hierarchy = cv2.findContours(binary, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-# Find minimum value to normalize with
-min_val = 1000000000
-max_val = 0
-for i in range(len(contours)):
-    for j in range(len(contours[i])):
-        if contours[i][j][0][0] < min_val:
-            min_val = contours[i][j][0][0]
-
-        #if contours[i][j][0][1] < min_val:
-        #    min_val = contours[i][j][0][1]
-
-        if contours[i][j][0][0] > max_val:
-            max_val = contours[i][j][0][0]
-
-        #if contours[i][j][0][1] > max_val:
-        #    max_val = contours[i][j][0][1]
-
-diff = (max_val - min_val) / 20
-print(len(contours))
+# Set origin and resolution of obstacles
+x_origin = -12
+y_origin = -35
+resolution = 0.05
 for i in range(len(contours)):
     for j in range(len(contours[i]) - 1):
-        x1 = contours[i][j][0][0] / diff
-        y1 = contours[i][j][0][1] / diff
-        x2 = contours[i][j+1][0][0] / diff
-        y2 = contours[i][j+1][0][1] / diff
-        print('<obstacle x1="' + str(x1) + '" y1="' + str(y1) + '" x2="' + str(x2) + '" y2="' + str(y2) + '"/>')
+        x1 = contours[i][j][0][0] * resolution + x_origin
+        y1 = contours[i][j][0][1] * resolution + y_origin
+        x2 = contours[i][j+1][0][0] * resolution + x_origin
+        y2 = contours[i][j+1][0][1] * resolution + y_origin
+        contents.insert(obstacle_insert_line, '\t<obstacle x1="' + str(x1) + '" y1="' + str(y1) + '" x2="' + str(x2) + '" y2="' + str(y2) + '"/>\n')
+
+# Seek back to start of file and then override with new content
+f.seek(0)
+f.writelines(contents)
+f.truncate()
+f.close()
+
+# Display image showing contours
 image = cv2.drawContours(image, contours, -1, (0,255,0), 2)
 plt.imshow(image)
 plt.show()
